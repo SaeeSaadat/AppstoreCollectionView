26377dcb24b4b5c9b2cba0f3181f4e92d9b385ea
Saee <saee_saadat@outlook.com>
2021-10-06 09:48:34 +0330

    main storyboard

---
+++ AppstoreCollectionView/AppsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AppsViewController.swift
+//  AppstoreCollectionView
+//
+//  Created by Saee Saadat on 10/6/21.
+//
+
+import UIKit
+
+class AppsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
